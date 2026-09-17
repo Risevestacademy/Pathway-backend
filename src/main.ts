0107dde43893d47e5f99d6569b08a05be9220c50
Donald Ogbe <ogbeebimobowei@gmail.com>
2026-09-17 21:31:08 +0100

    feat: enable global validation pipe

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,17 @@ import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { EnvironmentVariables } from './config';
+import { createValidationPipe } from './common/pipes/validation.pipe';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   const config =
     app.get<ConfigService<EnvironmentVariables, true>>(ConfigService);
 
+  app.useGlobalPipes(createValidationPipe());
+
   await app.listen(config.get('PORT', { infer: true }));
 }
+
 bootstrap();
